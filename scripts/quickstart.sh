7e7ac6e4321ef8044c83a1a7d97fec91aaeeca21
Andrew-Pohl <55916961+Andrew-Pohl@users.noreply.github.com>
2020-11-12 09:46:25 +0000

    Made some changes to the quickstart (#64)
    * Made some changes to the quickstart
    
    1. Added threading parameters to the parity node for the number of threads to create these are set using the env file or default to 1 and 4 if not set
    2. Added an optional node name for the validators to make it easier to change the display name in netstats (health page)
    
    * make NUM_RPC_THREADS and NUM_HTTP_THREADS default scale with CPU cores
    
    NUM_RPC_THREADS  = cpu cores
    NUM_HTTP_THREADS  = 4* cpu cores
    
    Add message in env with recommended settings

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -245,6 +245,7 @@ function parseArguments {
       echo "Warning! trying to run a bootnode without BOOTNODES argument!"
     fi
   fi
+
 }
 
 function checkRoleArgument {
@@ -457,6 +458,27 @@ function run {
     "node")
       INSTANCE_NAME=$NODE_KEY
 
+      ## parse parity config
+      cpuCores=1
+      if [ $PLATFORM == "LINUX" ]; then
+        cpuCores=$(nproc --all)
+      fi
+      NUM_RPC_THREADS=cpuCores
+      NUM_HTTP_THREADS=$(( 4*cpuCores ))
+      if [ -z "$NUMBER_OF_RPC_THREADS" ] ; then
+      	echo "using default RPC thread values"
+      else
+      	NUM_RPC_THREADS=$NUMBER_OF_RPC_THREADS
+        echo "reading RPC threads from env file $NUM_RPC_THREADS"
+      fi
+
+      if [ -z "$NUMBER_OF_HTTP_CONNECTIONS_THREADS" ] ; then
+      	echo "using default http thread values"
+      else
+        NUM_HTTP_THREADS=$NUMBER_OF_HTTP_CONNECTIONS_THREADS
+        echo "reading HTTP connection threads from env file $NUM_HTTP_THREADS"
+      fi
+
       ## Start parity container with all necessary arguments.
       $PERMISSION_PREFIX docker run \
         $DOCKER_LOG_OPTS \
@@ -471,7 +493,7 @@ function run {
         --restart=always \
         $DOCKER_IMAGE_PARITY \
         --role node \
-        --parity-args --no-warp --node-key $NODE_KEY
+        --parity-args --no-warp --node-key $NODE_KEY --jsonrpc-threads $NUM_RPC_THREADS --jsonrpc-server-threads $NUM_HTTP_THREADS
       ;;
 
     "validator")
@@ -479,6 +501,13 @@ function run {
       local address=$(cat $ADDRESS_FILE)
 
       INSTANCE_NAME=$address
+      if [ -z "$VAL_NAME" ] ; then
+	echo "using the address as the netstats name to update this pull the latest env file and set the VAL_NAME variable"
+      else
+	echo "setting netstats name to $VAL_NAME"
+	INSTANCE_NAME=$VAL_NAME
+      fi
+
 
       ## Start parity container with all necessary arguments.
       $PERMISSION_PREFIX docker run \
