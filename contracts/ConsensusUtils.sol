c886bc75912a8ff1625a167e91d51e9cc2cf4263
Andrew Pohl <andypohl@hotmail.co.uk>
2021-03-27 10:55:58 +0000

    Add maintenance tagging

--- contracts/ConsensusUtils.sol
+++ contracts/ConsensusUtils.sol
@@ -400,6 +400,11 @@ contract ConsensusUtils is EternalStorage, ValidatorSet {
     _addJailedVal(_address);
   }
 
+  function _maintenance(address _address) internal {
+    _pendingValidatorsRemove(_address);
+    _addJailedVal(_address);
+  }
+
   function _setPendingValidatorsAtPosition(uint256 _p, address _address) internal {
     addressArrayStorage[PENDING_VALIDATORS][_p] = _address;
   }
